075a1de9b4be992dc17d8c9f325dcdd4532500bf
Adil Ahmed Laskar <88890431+FNEX0P@users.noreply.github.com>
2021-09-22 19:20:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background("yellow");
+if(keyIsDown(DOWN_ARROW)){
+  background("orange")
+}
+if(keyIsDown(UP_ARROW)){
+  background("red")
+}
+if(keyIsDown(LEFT_ARROW)){
+  background("pink")
+}
+if(keyIsDown(RIGHT_ARROW)){
+  background("blue")
+}
+
+
+
+
+
+}
+
+
+
+
